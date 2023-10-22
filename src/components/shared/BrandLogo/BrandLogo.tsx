753d07983d148066e3337b632db78f59d044ab14
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-10-22 05:36:56 +0100

    added BrandLogo Component

---
+++ src/components/shared/BrandLogo/BrandLogo.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+const BrandLogo = (props: Props) => {
+  return (
+    <>
+      <h3 className="font-oleo font-normal text-3xl md:text-4xl text-black dark:text-neutral-200 ">
+        Gen-Z
+      </h3>
+    </>
+  );
+};
+
+export default BrandLogo;
