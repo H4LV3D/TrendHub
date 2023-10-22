3480e2ba75cfeaf40606e87a20c982d724eb2868
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-10-22 06:09:10 +0100

    update Collaborative.tsx and Tetiary.tsx

--- src/components/shared/buttons/Tetiary.tsx
+++ src/components/shared/buttons/Tetiary.tsx
@@ -19,7 +19,7 @@ function TetiaryButton({ text, link, small, active, action }: Props) {
             small
               ? "text-sm text-neutral-400 hover:text-black"
               : "text-lg p-2 text-neutral-400 dark:text-neutral-400 hover:text-black"
-          } ${active ? "!text-black font-medium dark:text-neutral-400" : ""} `}
+          } ${active ? "text-black font-medium dark:text-neutral-300" : ""} `}
         >
           {text}
         </Link>
