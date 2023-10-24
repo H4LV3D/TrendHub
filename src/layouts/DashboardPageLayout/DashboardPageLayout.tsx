298413dc4918f5aba8e1887c2b07a5c7a0ab5f6e
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-10-24 13:13:51 +0100

    update DashboardPageLayout.tsx

--- src/layouts/DashboardPageLayout/DashboardPageLayout.tsx
+++ src/layouts/DashboardPageLayout/DashboardPageLayout.tsx
@@ -1,27 +1,25 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
+import Loader from "@/components/shared/Loader/Loader";
 import Navbar from "@/components/shared/Navbar/Navbar";
 import Header from "./Header/Header";
 import Footer from "@/components/shared/footer/Footer";
-import ModalProvider from "@/components/page-wrappers/ModalProvider/ModalProvider";
 
-type Props = {
+interface Props {
   children: React.ReactNode;
-};
+}
 
-const ProtectedPageLayout = ({ children }: Props) => {
+const ProtectedPageLayout: React.FC<Props> = ({ children }) => {
   return (
     <>
-      <ModalProvider>
-        <div className="bg-white dark:bg-black/90 min-h-screen w-full">
-          <Navbar nav={true} />
-          <div className="mt-20 mb-12">
-            <Header />
-            {children}
-          </div>
-          <Footer />
+      <div className="bg-white dark:bg-black/90 min-h-screen w-full">
+        <Navbar nav={true} />
+        <div className="mt-20 mb-12">
+          <Header />
+          {children}
         </div>
-      </ModalProvider>
+        <Footer />
+      </div>
     </>
   );
 };
