7b9db9a7e5315fb01c3e8c5d623fbf4e429dfcaf
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-10-24 15:23:12 +0100

    update Banner.tsx

--- src/components/shared/Banner/Banner.tsx
+++ src/components/shared/Banner/Banner.tsx
@@ -18,13 +18,13 @@ const Banner = ({ heading, text, data }: Props) => {
         id="search"
       >
         <div className="flex flex-col justify-center items-center mt-20 lg:w-[900px]">
-          <h1 className="text-5xl sm:text-7xl md:text-7xl lg:text-8xl text-black dark:text-neutral-200 font-oleo font-normal  text-center">
+          <h1 className="text-4xl xs:text-5xl sm:text-6xl md:text-[5rem] xl:text-8xl text-black dark:text-neutral-200 font-oleo font-normal sm:text-center">
             {heading.line1}
             <br className="xs:hidden md:block" /> {heading.line2}
           </h1>
         </div>
         <div className="w-full flex flex-col justify-center items-center mt-7 lg:w-[900px]">
-          <p className="text-sm sm:text-base md:text-lg text-center font-raleway font-normal dark:text-neutral-400 text-gray-800 sm:w-2/3 lg:w-2/3 mb-8">
+          <p className="text-sm sm:text-base md:text-lg sm:text-center font-raleway font-normal dark:text-neutral-400 text-gray-800 md:w-2/3 mb-8">
             {text}
           </p>
         </div>
