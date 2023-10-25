9c03d223dce888d72c7f14c1784fa6401b309f5f
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-10-25 08:45:35 +0100

    update BrandLogo.tsx

--- src/components/shared/BrandLogo/BrandLogo.tsx
+++ src/components/shared/BrandLogo/BrandLogo.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import Link from "next/link";
 
 type Props = {};
 
 const BrandLogo = (props: Props) => {
   return (
     <>
-      <h3 className="font-oleo font-normal text-3xl md:text-4xl text-black dark:text-neutral-200 ">
-        Gen-Z
-      </h3>
+      <Link href="/">
+        <button className="font-oleo font-normal text-3xl md:text-4xl text-black dark:text-neutral-200 ">
+          Gen-Z
+        </button>
+      </Link>
     </>
   );
 };
