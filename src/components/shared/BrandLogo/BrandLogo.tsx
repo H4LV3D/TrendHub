f6360c7ee9519c1ae3587b4ad12b4ee0a149b9dc
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-10-29 21:09:26 +0100

    update BrandLogo.tsx

--- src/components/shared/BrandLogo/BrandLogo.tsx
+++ src/components/shared/BrandLogo/BrandLogo.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Link from "next/link";
+import { useAppDispatch } from "@/hooks/useAppDispatch";
+import { hideMobileSidebar } from "@/store/slices/mobileSidebar/mobileSidebarSlice";
 
 type Props = {};
 
 const BrandLogo = (props: Props) => {
+  const dispatch = useAppDispatch();
   return (
     <>
-      <Link href="/">
+      <Link href="/" onClick={() => dispatch(hideMobileSidebar())}>
         <button className="font-oleo font-normal text-3xl md:text-4xl text-black dark:text-neutral-200 ">
           Gen-Z
         </button>
