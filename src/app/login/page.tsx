890581b89ef6fb433d46a18b091990a0c00852b2
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-10-30 06:26:34 +0100

    🦄 refactor: clean up login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -12,24 +12,6 @@ const Login = (props: Props) => {
   const [message, setMessage] = React.useState("");
   return (
     <>
-      {notify && (
-        <div className="fixed top-8 left-1/2 transform -translate-x-1/2 w-[300px] md:w-[450px] z-50 font-raleway">
-          <div className="w-full bg-black dark:bg-gray-100 flex items-center space-x-3 p-3 rounded-lg">
-            <div className="w-[60px] flex justify-center text-gray-100 dark:text-black">
-              <i className="fas fa-gift fa-xl fa-fw"></i>
-            </div>
-            <div className="font-raleway">
-              <h3 className="font-semibold text-lg font-raleway text-white dark:text-black">
-                {message}
-              </h3>
-              <p className="text-sm font-raleway text-gray-100 dark:text-black">
-                You have successfully been able to subscribe to my blog,
-                newsletter and updates feed for more interesting episodes.
-              </p>
-            </div>
-          </div>
-        </div>
-      )}
       <AuthLayout form="login">
         <LoginForm setNotify={setNotify} setMessage={setMessage} />
         <div className="mt-6 flex flex-col space-y-2">
