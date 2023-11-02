81622d2abd0abddd6521b255ca3ff6b6b15d562a
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-11-02 23:46:55 +0100

    update Banner.tsx

--- src/components/shared/Banner/Banner.tsx
+++ src/components/shared/Banner/Banner.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchBox from "@/components/shared/search/Search";
+import { motion } from "framer-motion";
 
 type Props = {
   heading: {
@@ -18,10 +19,16 @@ const Banner = ({ heading, text, data }: Props) => {
         id="search"
       >
         <div className="flex flex-col justify-center items-center mt-20 lg:w-[900px]">
-          <h1 className="text-4xl xs:text-5xl sm:text-6xl md:text-[5rem] xl:text-8xl text-black dark:text-neutral-200 font-oleo font-normal sm:text-center">
+          <motion.h1
+            initial={{ x: -100, opacity: 0.3 }}
+            animate={{ x: 0, opacity: 1 }}
+            exit={{ x: 100, opacity: 0.5 }}
+            transition={{ duration: 1 }}
+            className="text-4xl xs:text-5xl sm:text-6xl md:text-[5rem] xl:text-8xl text-black dark:text-neutral-200 font-oleo font-normal sm:text-center"
+          >
             {heading.line1}
             <br className="xs:hidden md:block" /> {heading.line2}
-          </h1>
+          </motion.h1>
         </div>
         <div className="w-full flex flex-col justify-center items-center mt-7 lg:w-[900px]">
           <p className="text-sm !leading-6 md:!leading-7 sm:text-base md:text-lg sm:text-center font-raleway font-normal dark:text-neutral-400 text-gray-800 md:w-2/3 mb-8">
