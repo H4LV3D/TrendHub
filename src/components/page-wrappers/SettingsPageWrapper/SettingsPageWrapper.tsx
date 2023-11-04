230896b7f4eae0b5bdac5f8ca9a4cdf768c069f5
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-11-04 11:09:55 +0100

    update SettingsPageWrapper.tsx

--- src/components/page-wrappers/SettingsPageWrapper/SettingsPageWrapper.tsx
+++ src/components/page-wrappers/SettingsPageWrapper/SettingsPageWrapper.tsx
@@ -3,13 +3,27 @@ import React, { useState } from "react";
 import MaxWidthProvider from "@/components/shared/MaxWidthProvider/MaxWidthProvider";
 
 type Props = {};
+const settings = [
+  "Personal Details",
+  "Personalize",
+  "Customize Theme",
+  "Change Password",
+  "Notifications",
+  "Collaboration",
+  "Request Access",
+  "Delete Account",
+];
 
-const About = ({}: Props) => {
+// personalize is about setting a preffered color, memoji and setting a custom qr code linked to your profile
+// connect to apple books so i can sync reading time
+
+const SettingsPageWrapper = ({}: Props) => {
+  const [active, setActive] = useState("Personalize");
   return (
     <>
       <MaxWidthProvider>
         <div className="">
-          <div className="bg-white dark:bg-[#191919] h-[55vh] w-full flex flex-col items-center justify-center">
+          {/* <div className="bg-white dark:bg-[#191919] h-[55vh] w-full flex flex-col items-center justify-center">
             <div className="flex flex-col justify-center items-center mt-20 lg:w-[900px]">
               <h1 className="text-5xl sm:text-6xl md:text-7xl xl:text-7xl text-black dark:text-neutral-200 font-oleo font-normal  text-center">
                 Settings
@@ -20,23 +34,36 @@ const About = ({}: Props) => {
                 Adjust your settings to your liking.
               </p>
             </div>
+          </div> */}
+          <div className="bg-white dark:bg-[#191919] h-[20vh] w-full flex items-center">
+            <div>
+              <h1 className="text-3xl text-black dark:text-neutral-200 font-raleway font-medium">
+                Settings
+              </h1>
+              <p className="text-sm sm:text-base font-raleway font-normal dark:text-neutral-400 text-gray-800 ">
+                Adjust your settings to your liking.
+              </p>
+            </div>
           </div>
 
-          <hr className="w-full border-gray-200 dark:border-neutral-800" />
-
-          <div className="bg-white dark:bg-[#191919] h-[55vh] w-full mt-4">
-            <div className="grid grid-cols-2 gap-4 mb-8">
-              {/* {Array(4).fill(
-                <div className="flex flex-col border p-5 rounded-md ">
-                  <h3 className="font-raleway font-normal text-lg text-black dark:text-neutral-400 mb-4">
-                    Resource
-                  </h3>
-                  <p className="">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quibusdam, voluptate.
-                  </p>
-                </div>
-              )} */}
+          <div className="bg-white dark:bg-[#191919] w-full p-5 flex border rounded-lg md:mb-[20vh] ">
+            <div className="grid grid-cols-1 w-1/3 pr-6 py-6 border-r ">
+              {settings.map((setting) => (
+                <button
+                  className={`font-raleway font-normal text-lg text-start text-black dark:text-neutral-400 p-3 ${
+                    active === setting
+                      ? "border border-black rounded-lg"
+                      : "border-b"
+                  }`}
+                  onClick={() => setActive(setting)}
+                  key={setting}
+                >
+                  {setting}
+                </button>
+              ))}
+            </div>
+            <div className="w-2/3 p-6 h-full ">
+              <h2 className="font-medium text-2xl">{active}</h2>
             </div>
           </div>
         </div>
@@ -45,4 +72,4 @@ const About = ({}: Props) => {
   );
 };
 
-export default About;
+export default SettingsPageWrapper;
