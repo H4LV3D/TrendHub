6f33caff6725b1e144936d7ec17faf151fa3d43a
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-11-04 11:09:33 +0100

    update DashboardPageWrapper.tsx

--- src/components/page-wrappers/DashboardPageWrapper/DashboardPageWrapper.tsx
+++ src/components/page-wrappers/DashboardPageWrapper/DashboardPageWrapper.tsx
@@ -4,39 +4,25 @@ import MaxWidthProvider from "@/components/shared/MaxWidthProvider/MaxWidthProvi
 
 type Props = {};
 
-const About = ({}: Props) => {
+const DashboardPageWrapper = ({}: Props) => {
   return (
     <>
       <MaxWidthProvider>
-        <div className="md:container mx-auto px-6 lg:px-0 font-raleway mb-12">
-          <div className="bg-white dark:bg-[#191919] h-[55vh] w-full flex flex-col items-center justify-center">
-            <div className="flex flex-col justify-center items-center mt-20 lg:w-[900px]">
-              <h1 className="text-5xl sm:text-6xl md:text-7xl xl:text-7xl text-black dark:text-neutral-200 font-oleo font-normal  text-center">
-                Dashboard
-              </h1>
-            </div>
-            <div className="w-full flex flex-col justify-center items-center mt-7 lg:w-[900px]">
-              <p className="text-sm sm:text-base md:text-lg text-center font-raleway font-normal dark:text-neutral-400 text-gray-800 sm:w-3/4 lg:w-2/3 mb-8">
-                Enjoy your space.
-              </p>
-            </div>
-          </div>
-
-          <hr className="w-full border-gray-200 dark:border-neutral-800" />
-
+        <div className="md:container mx-auto px-6 lg:px-0 font-raleway my-8">
           <div className="bg-white dark:bg-[#191919] h-[55vh] w-full mt-4">
-            <div className="grid grid-cols-2 gap-4 mb-8">
-              {/* {Array(4).fill(
+            <h3 className="font-medium text-xl mb-3">Recent Blogs</h3>
+            <div className="grid grid-cols-4 gap-4 mb-8">
+              {Array(4).fill(
                 <div className="flex flex-col border p-5 rounded-md ">
-                  <h3 className="font-raleway font-normal text-lg text-black dark:text-neutral-400 mb-4">
+                  <h3 className="font-raleway font-[500] text-lg text-black dark:text-neutral-400 mb-2">
                     Resource
                   </h3>
-                  <p className="">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  <p className="text-base">
+                    Lorem ipsum dolor sit amet conse tetur adipisicing elit.
                     Quibusdam, voluptate.
                   </p>
                 </div>
-              )} */}
+              )}
             </div>
           </div>
         </div>
@@ -45,4 +31,4 @@ const About = ({}: Props) => {
   );
 };
 
-export default About;
+export default DashboardPageWrapper;
