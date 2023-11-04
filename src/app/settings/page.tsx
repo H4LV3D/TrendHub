f38921ad665cce65d815575bcd0bcd125ac2dfb5
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-11-04 11:09:28 +0100

    update page.tsx

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import ProtectedPageLayout from "@/layouts/ProtectedPageLayout/ProtectedPageLayout";
 import DashboardPageLayout from "@/layouts/DashboardPageLayout/DashboardPageLayout";
 import SettingsPageWrapper from "@/components/page-wrappers/SettingsPageWrapper/SettingsPageWrapper";
@@ -7,13 +7,13 @@ import Head from "next/head";
 
 type Props = {};
 
-const About = ({}: Props) => {
+const Settings = ({}: Props) => {
   return (
     <>
       <>
         <Head>
-          <title>Gen-Z | Dashboard</title>
-          <meta name="description" content="Klanera dashboard" />
+          <title>Gen-Z | Settings</title>
+          <meta name="description" content="Gen-Z Blog settings" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.png" />
         </Head>
@@ -25,4 +25,4 @@ const About = ({}: Props) => {
   );
 };
 
-export default About;
+export default Settings;
