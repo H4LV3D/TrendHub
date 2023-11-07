59ade7cc642c08f2b8ffa05a83ceeaa20d71b7f3
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-11-07 18:12:51 +0100

    update Secondary.tsx

--- src/components/shared/buttons/Secondary.tsx
+++ src/components/shared/buttons/Secondary.tsx
@@ -5,14 +5,14 @@ type Props = {
   text: String;
   type?: "button" | "submit" | "reset" | undefined;
   action?: () => void;
-  link: string;
+  link?: string;
 };
 
 function SecondaryButton({ text, type, action, link }: Props) {
   if (!link) {
     return (
       <button
-        className="border dark:border-neutral-700 hover:border-black rounded-lg w-full py-3 flex items-center justify-center text-neutral-400 text-sm md:text-base hover:text-black mb-3"
+        className="border dark:border-neutral-700 hover:border-black rounded-lg w-full h-[3.5rem] flex items-center justify-center text-neutral-400 text-sm md:text-base hover:text-black"
         type={type}
         onClick={action}
       >
@@ -23,7 +23,7 @@ function SecondaryButton({ text, type, action, link }: Props) {
   return (
     <>
       <Link href={link}>
-        <div className="text-neutral-400 dark:text-neutral-600 dark:hover:text-neutral-400 text-center hover:text-black border dark:border-neutral-800 dark:hover:border-neutral-500 border-neutral-400 hover:border-black rounded-lg w-full text-base md:text-lg py-2.5 px-6">
+        <div className="w-full text-neutral-400 dark:text-neutral-600 dark:hover:text-neutral-400 text-center hover:text-black border dark:border-neutral-800 dark:hover:border-neutral-500 border-neutral-400 hover:border-black rounded-lg text-base md:text-lg h-[3.5rem] px-6">
           {text}
         </div>
       </Link>
