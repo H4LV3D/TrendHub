86de29f4b131e3e83f45b89de11afbf5950913cc
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-11-11 22:36:48 +0100

    Refactor MyBlogsPageWrapper component layout

--- src/components/page-wrappers/MyBlogsPageWrapper/MyBlogsPageWrapper.tsx
+++ src/components/page-wrappers/MyBlogsPageWrapper/MyBlogsPageWrapper.tsx
@@ -8,20 +8,18 @@ const MyBlogsPageWrapper = ({}: Props) => {
   return (
     <>
       <MaxWidthProvider>
-        <div className="">
-          <div className="bg-white dark:bg-[#191919] h-[20vh] w-full flex items-center">
-            <div>
-              <h1 className="text-3xl text-black dark:text-neutral-200 font-raleway font-medium">
-                My Blogs
-              </h1>
-              <p className="text-sm sm:text-base font-raleway font-normal dark:text-neutral-400 text-gray-800 ">
-                Read, Schedule and Outline your blog article here.
-              </p>
-            </div>
+        <div className="px-6 lg:px-0">
+          <div className="bg-white dark:bg-[#191919] py-10 md:h-[20vh] w-full flex flex-col items-start md:items-center">
+            <h1 className="text-3xl text-black dark:text-neutral-200 font-raleway font-medium">
+              My Blogs
+            </h1>
+            <p className="text-sm sm:text-base font-raleway font-normal dark:text-neutral-400 text-gray-800 ">
+              Read, Schedule and Outline your blog article here.
+            </p>
           </div>
 
-          <div className="bg-white dark:bg-[#191919] w-full flex rounded-lg md:mb-[20vh] ">
-            <div className="w-1/2 pr-6 h-full">
+          <div className="bg-white dark:bg-[#191919] w-full md:flex rounded-lg md:mb-[20vh] ">
+            <div className="w-full md:w-1/2 pr-6 h-full">
               <h2 className="font-medium text-xl mb-2">Bookmarks</h2>
               <hr />
               <div className="border-b p-4">
@@ -34,7 +32,7 @@ const MyBlogsPageWrapper = ({}: Props) => {
                 </p>
               </div>
             </div>
-            <div className="grid grid-cols-1 w-1/2 pl-6 pb-12 border-l">
+            <div className="grid grid-cols-1 w-full md:w-1/2 md:pl-6 pb-12 md:border-l">
               <h2 className="font-medium text-xl mb-2">Trending</h2>
               <div className="border-y p-4">
                 <h3 className="font-raleway font-[500] text-lg text-black dark:text-neutral-400 mb-1">
