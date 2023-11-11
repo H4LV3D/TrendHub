0aca26df03e653c0448fdfca45c689d2f6548faa
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-11-11 22:36:34 +0100

    Refactor SettingsPageWrapper component layout

--- src/components/page-wrappers/SettingsPageWrapper/SettingsPageWrapper.tsx
+++ src/components/page-wrappers/SettingsPageWrapper/SettingsPageWrapper.tsx
@@ -63,7 +63,7 @@ const SettingsPageWrapper = ({}: Props) => {
   return (
     <>
       <MaxWidthProvider>
-        <div className="">
+        <div className="px-6 sm:px-0">
           <div className="bg-white dark:bg-[#191919] h-[20vh] w-full flex items-center">
             <div>
               <h1 className="text-3xl text-black dark:text-neutral-200 font-raleway font-medium">
@@ -75,23 +75,45 @@ const SettingsPageWrapper = ({}: Props) => {
             </div>
           </div>
 
-          <div className="bg-white dark:bg-[#191919] w-full min-h-[75vh] p-5 flex border rounded-xl md:mb-[20vh] ">
-            <div className="grid grid-cols-1 w-1/3 pr-6 py-12 border-r ">
+          <div className="bg-white dark:bg-[#191919] w-full md:min-h-[75vh] sm:p-5 flex md:border rounded-xl md:mb-[20vh] ">
+            <div className="grid grid-cols-1 w-full md:w-1/3 md:pr-6 md:py-12 md:border-r ">
               {Settings.map((setting) => (
-                <button
-                  className={`font-raleway font-normal text-lg text-start text-black dark:text-neutral-400 p-3 ${
+                <div
+                  className={`font-raleway font-normal text-lg flex flex-col md:items-center md:justify-between ${
                     active === setting.id
                       ? "border border-black rounded-lg"
-                      : "border-b"
-                  }`}
-                  onClick={() => setActive(setting.id)}
-                  key={setting.title}
+                      : ""
+                  } `}
                 >
-                  {setting.title}
-                </button>
+                  <button
+                    className={`text-start text-black dark:text-neutral-400 w-full p-3 flex items-center justify-between ${
+                      active === setting.id
+                        ? "border-b md:border-0 border-black"
+                        : "border-b "
+                    }`}
+                    onClick={() => setActive(setting.id)}
+                    key={setting.title}
+                  >
+                    {setting.title}
+                    {active === setting.id && (
+                      <div className="md:hidden">
+                        <i
+                          className="fas fa-chevron-down fa-sm fa-fw"
+                          aria-hidden="true"
+                        ></i>
+                      </div>
+                    )}
+                  </button>
+
+                  {active === setting.id && (
+                    <div className="md:hidden p-3 flex items-start">
+                      <h3 className="text-lg">{setting.title}</h3>
+                    </div>
+                  )}
+                </div>
               ))}
             </div>
-            <div className="w-2/3 p-6 h-full ">
+            <div className="hidden md:w-2/3 p-6 h-full ">
               <div className="">
                 <h2 className="font-medium text-2xl mb-1">
                   {currentSetting?.title}
