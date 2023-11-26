03621809451f1f294c750d0808bc18a69efc99e2
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-11-26 23:45:11 +0100

    Update padding in SuggestedInfo component

--- src/components/shared/SuggestedInfo/SuggestedInfo.tsx
+++ src/components/shared/SuggestedInfo/SuggestedInfo.tsx
@@ -30,7 +30,7 @@ const SuggestedInfo = ({ data, split }: Props) => {
   const splitData = split ? split : 4;
   return (
     <>
-      <div className="md:container sm:w-[450px] md:w-[650px] lg:w-[700px] mx-auto xs:px-0 px-6 pb-6">
+      <div className="md:container sm:w-[450px] md:w-[650px] lg:w-[700px] mx-auto xs:px-0 px-8 pb-6">
         <div className="py-4 md:grid grid-cols-2 gap-x-6">
           {dataCopy.splice(0, splitData).map((blog: any, index: number) => (
             <div key={index} className="mb-6">
