a316f29a1c1fec0d052d55da63be5f2173279ebd
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-11-26 23:45:05 +0100

    Update Navbar styling

--- src/components/shared/Navbar/Navbar.tsx
+++ src/components/shared/Navbar/Navbar.tsx
@@ -58,7 +58,7 @@ function Header({ nav }: Props) {
   const headerClasses = `font-raleway flex items-center justify-between py-3  ${
     nav
       ? `lg:border-b border-neutral-50 dark:border-[#191919] md:container mx-auto`
-      : "border-b border-neutral-100 dark:border-[#191919] sm:border-none px-6"
+      : "border-b border-neutral-100 dark:border-[#191919] sm:border-none px-8"
   } ${hasShadow ? "shadow-sm border-gray-200" : ""}`;
 
   return (
