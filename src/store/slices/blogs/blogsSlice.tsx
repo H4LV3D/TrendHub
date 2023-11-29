ff8e50f05a495b81495235f9c6e9bb255ffb1444
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-11-29 11:06:54 +0100

    Add Blog interface to blogsSlice.tsx

--- src/store/slices/blogs/blogsSlice.tsx
+++ src/store/slices/blogs/blogsSlice.tsx
@@ -1,25 +1,8 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import pageData from "@/data/index.json";
+import { Blog } from "@/typings/blog";
 
-type Blog = {
-  title: string;
-  episode?: string;
-  description?: string;
-  readTime: string;
-  date: string;
-  article: {
-    p: string;
-  }[];
-  link: string | number;
-  image: string;
-  tags?: string[];
-  reactions?: {
-    good: number;
-    bad: number;
-  };
-};
-
-interface ArrangementState {
+export interface ArrangementState {
   value: Blog[];
   show: boolean;
 }
