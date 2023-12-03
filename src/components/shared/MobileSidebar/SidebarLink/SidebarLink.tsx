46dbecb5f877bd9a3f4b184b95c9dd0c829832df
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-12-03 14:41:58 +0100

    Update styles for active sidebar link

--- src/components/shared/MobileSidebar/SidebarLink/SidebarLink.tsx
+++ src/components/shared/MobileSidebar/SidebarLink/SidebarLink.tsx
@@ -21,7 +21,7 @@ const SidebarLink: React.FC<Props> = ({ text, href }) => {
         href={href}
         className={`flex items-center group relative gap-6 h-12 px-4 rounded-xl dark:text-neutral-400 ${
           isActive &&
-          "dark:text-white bg-neutral-200 dark:bg-neutral-700 font-bold"
+          "dark:text-white bg-black text-white dark:bg-neutral-700 font-bold"
         }`}
       >
         <p className={`font-[500] text-sm sm:text-base whitespace-nowrap `}>
