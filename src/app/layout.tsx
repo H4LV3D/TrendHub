730e766c5a6a74c3c7436789bfacbff6c225e296
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-12-03 14:40:12 +0100

    Refactor layout component and update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,16 @@
 "use client";
 import "./globals.css";
-import type { Metadata } from "next";
 import { Raleway, Poppins } from "next/font/google";
 import { ThemeProvider } from "@/contexts/themeContext";
 import { Provider } from "react-redux";
 import { store } from "@/store/store";
+import type { Metadata } from "next";
 import Head from "next/head";
 
 const raleway = Raleway({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
 });
-
-const poppins = Poppins({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["latin"],
-});
-
-const metadata: Metadata = {
-  title: "Gen-Z Tales",
-  description: "Gen-Z Tales | Express your thoughts and opinions in words",
-};
 
 export default function RootLayout({
   children,
@@ -51,6 +41,10 @@ export default function RootLayout({
           href="/assets/fontawesome-free-6.2.1-web/css/fontawesome.css"
         />
         <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content="Gen-Z Tales | Express your thoughts and opinions in words"
+        />
       </head>
       <body className={`${raleway.className}`}>
         <Provider store={store}>
