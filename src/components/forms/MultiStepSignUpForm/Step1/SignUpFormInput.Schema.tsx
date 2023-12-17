5c3281f0bd334685b6cc07fbf197f692d94db0b1
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-12-17 07:37:58 +0100

    Update SignUpFormInput.Schema.tsx

--- src/components/forms/MultiStepSignUpForm/Step1/SignUpFormInput.Schema.tsx
+++ src/components/forms/MultiStepSignUpForm/Step1/SignUpFormInput.Schema.tsx
@@ -1,12 +1,13 @@
 import * as yup from "yup";
 
 export const signUpSchema = yup.object().shape({
-  firstName: yup.string().required("First name is required"),
-  lastName: yup.string().required("Last name is required"),
+  // firstName: yup.string().required("First name is required"),
+  // lastName: yup.string().required("Last name is required"),
+  fullName: yup.string().required("Full name is required"),
   email: yup.string().email("Invalid email").required("Email is required"),
   password: yup
     .string()
     .required("Password is required")
     .min(8, "Password must be at least 8 characters"),
-  country: yup.string().required("Country is required"),
+  avatarId: yup.number().required("avatarId is required"),
 });
