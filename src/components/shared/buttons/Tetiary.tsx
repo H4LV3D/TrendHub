533f17ab4c9c19f99a558cb67dd96e32abe5ae0e
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-12-19 06:29:15 +0100

    Fix onClick function in TetiaryButton component

--- src/components/shared/buttons/Tetiary.tsx
+++ src/components/shared/buttons/Tetiary.tsx
@@ -34,7 +34,7 @@ function TetiaryButton({ text, link, small, active, action }: Props) {
               ? "text-sm text-neutral-400 hover:text-black"
               : "text-lg p-2 text-neutral-400 dark:text-neutral-400 hover:text-black"
           } ${active ? "text-black font-medium dark:text-neutral-400" : ""} `}
-          onClick={() => action}
+          onClick={action}
         >
           {text}
         </button>
