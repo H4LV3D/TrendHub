0a1ff8bb37de43cd8144eda2cd99d15a2ec6cf6b
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-12-19 06:28:45 +0100

    Add showOptions state and dropdown menu

--- src/app/blogs/[link]/sections/buttons.tsx
+++ src/app/blogs/[link]/sections/buttons.tsx
@@ -1,11 +1,13 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 
 type Props = {};
 
 const Buttons = (props: Props) => {
   const [liked, setLiked] = React.useState(false);
   const [bookmarked, setBookmarked] = React.useState(false);
+
+  const [showOptions, setShowOptions] = useState(false);
   return (
     <div>
       <div className="w-full mt-6">
@@ -51,12 +53,53 @@ const Buttons = (props: Props) => {
               </div>
             </div>
             <div className="group relative">
-              <button className="flex items-center justify-center h-10 w-10 rounded-md text-neutral-700 dark:text-neutral-400  cursor-pointer hover:bg-[#e7e7e7] dark:hover:bg-neutral-800 hover:text-neutral-700 dark:hover:text-neutral-300">
+              <button
+                onClick={() => setShowOptions(!showOptions)}
+                className="flex items-center justify-center h-10 w-10 rounded-md text-neutral-700 dark:text-neutral-400  cursor-pointer hover:bg-[#e7e7e7] dark:hover:bg-neutral-800 hover:text-neutral-700 dark:hover:text-neutral-300"
+              >
                 <i className="fa-solid fa-ellipsis fa-xl fa-fw"></i>
               </button>
-              <div className="opacity-0 bg-black text-white text-center text-xs font-raleway rounded-lg py-2 absolute z-40 group-hover:opacity-100 top-full -left-1 mt-4 px-4 pointer-events-none">
-                Options
-              </div>
+              {!showOptions && (
+                <div className="opacity-0 bg-black text-white text-center text-xs font-raleway rounded-lg py-2 absolute z-40 group-hover:opacity-100 top-full -left-1 mt-4 px-4 pointer-events-none">
+                  Options
+                </div>
+              )}
+              {showOptions && (
+                <div className="dropdown p-3 border bg-white drop-shadow-xl rounded-lg absolute top-12 right-0 w-[220px]">
+                  <ul className="text-neutral-700 text-[0.875rem] ">
+                    <li
+                      onClick={() => setShowOptions(!showOptions)}
+                      className="py-2 px-2 hover:bg-[#f7f7f7] hover:cursor-pointer  border-b"
+                    >
+                      Not interested in this
+                    </li>
+                    <li
+                      onClick={() => setShowOptions(!showOptions)}
+                      className="py-2 px-2 hover:bg-[#f7f7f7] hover:cursor-pointer  border-b"
+                    >
+                      Follow Toluwalope
+                    </li>
+                    <li
+                      onClick={() => setShowOptions(!showOptions)}
+                      className="py-2 px-2 hover:bg-[#f7f7f7] hover:cursor-pointer  border-b"
+                    >
+                      Mute Toluwalope
+                    </li>
+                    <li
+                      onClick={() => setShowOptions(!showOptions)}
+                      className="py-2 px-2 hover:bg-[#f7f7f7] hover:cursor-pointer  border-b"
+                    >
+                      Block Toluwalope
+                    </li>
+                    <li
+                      onClick={() => setShowOptions(!showOptions)}
+                      className="py-2 px-2 hover:bg-[#f7f7f7] hover:cursor-pointer "
+                    >
+                      Report post
+                    </li>
+                  </ul>
+                </div>
+              )}
             </div>
           </div>
         </div>
