fe6a338ab46f1529b659e1d7aed8615bb0631fc7
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-12-19 19:01:00 +0100

    Update metadata in page.tsx

--- src/app/blogs/[link]/page.tsx
+++ src/app/blogs/[link]/page.tsx
@@ -27,90 +27,41 @@ export async function generateMetadata(
   const previousKeywords = (await parent).keywords || [];
 
   return {
-    generator: "Gen-Z Tales | Trend Hub",
-    applicationName: "Gen-Z Tales | Trend Hub",
-    referrer: "origin-when-cross-origin",
+    title: `${selectedBlog.title} | Info Hub`,
+    description: selectedBlog.description,
     keywords: [
       "Gen-Z",
-      "Gen-Z Tales",
-      "Gen-Z Tech",
-      "Gen-Z Technology",
+      "Info Hub",
       ...selectedBlog.tags.map((tag) => tag),
       ...previousKeywords,
     ],
-    title: `${selectedBlog.title} | Gen-Z Tales`,
-    authors: [{ name: "Toluwalope Akinkunmi", url: "https://toluwalope.tech" }],
-    creator: "Toluwalope Akinkunmi",
-    publisher: "Toluwalope Akinkunmi",
-    formatDetection: {
-      email: true,
-      address: false,
-      telephone: true,
-    },
-    alternates: {
-      canonical: "/",
-      languages: {
-        "en-US": "/en-US",
-      },
-    },
-    metadataBase: new URL("https://trendingstuffs.vercel.app"),
     openGraph: {
-      title: `${selectedBlog.title} | Gen-Z Tales`,
+      title: `${selectedBlog.title} | Info Hub`,
       description: selectedBlog.description,
       url: "https://trendingstuffs.vercel.app",
-      siteName: "Trend Hub",
-      phoneNumbers: ["+234 815 943 3370"],
+      siteName: "Info Hub",
       type: "article",
       images: [
         {
           url: `https://treningstuffs.vercel.app${selectedBlog.image}`,
-          width: 800,
-          height: 800,
+          width: 600,
+          height: 600,
         },
         {
           url: `https://treningstuffs.vercel.app${selectedBlog.image}`,
-          width: 1800,
-          height: 1600,
+          width: 600,
+          height: 600,
           alt: "My custom alt",
         },
         ...previousImages,
       ],
-      locale: "en_US",
     },
-    robots: {
-      index: true,
-      follow: true,
-      nocache: true,
-      googleBot: {
-        index: true,
-        follow: true,
-        noimageindex: false,
-        "max-video-preview": -1,
-        "max-image-preview": "large",
-        "max-snippet": -1,
-      },
-    },
-    icons: {
-      icon: "/icon.png",
-      shortcut: "/shortcut-icon.png",
-      apple: "/apple-icon.png",
-      other: {
-        rel: "apple-touch-icon-precomposed",
-        url: "/apple-touch-icon-precomposed.png",
-      },
-    },
-    manifest: "https://trendingstuffs.vercel.app/manifest.json",
     twitter: {
       card: "summary_large_image",
-      title: `${selectedBlog.title} | Gen-Z Tales`,
+      title: `${selectedBlog.title} | Info Hub`,
       description: selectedBlog.description,
-      siteId: "1467726470533754880",
       creator: "@Kinkunmz_",
-      creatorId: "1467726470533754880",
       images: [`https://treningstuffs.vercel.app${selectedBlog.image}`],
-    },
-    verification: {
-      google: "6l2jGxL2QK9ZvqZj2rQ9KgQa2G4R1Xb6YJ3kzWZfz9k",
     },
   };
 }
