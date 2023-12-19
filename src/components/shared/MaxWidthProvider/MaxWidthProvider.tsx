aef735e5b3de6363a09ce397868a00a69209959b
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-12-19 19:01:33 +0100

    Update MaxWidthProvider styling

--- src/components/shared/MaxWidthProvider/MaxWidthProvider.tsx
+++ src/components/shared/MaxWidthProvider/MaxWidthProvider.tsx
@@ -7,7 +7,9 @@ interface Props {
 
 const MaxWidthProvider: React.FC<Props> = ({ className, children }) => {
   return (
-    <div className={`${className} max-w-[1244px] mx-auto px-6 md:px-8  `}>
+    <div
+      className={`${className} max-w-[1244px] mx-auto px-5 xs:px-4 lg:px-0  `}
+    >
       {children}
     </div>
   );
