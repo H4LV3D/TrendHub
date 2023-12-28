696e5129179b0d39dfb049333982c9781f3f0bee
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-12-28 10:40:27 +0100

    Refactor page.tsx to use HomePageWrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,54 @@
-"use client";
 import React from "react";
-import pageData from "@/data/index.json";
-import Genres from "./home/Genres";
-import Collaborative from "./home/Collaborative";
-import Review from "./home/Reviews";
-import Generations from "./home/Generations";
-import Subscribe from "./home/Subscribe";
-import Banner from "@/components/shared/Banner/Banner";
-import PageLayout from "@/layouts/PageLayout/PageLayout";
+import HomePageWrapper from "@/components/page-wrappers/HomePageWrapper/HomePageWrapper";
+
+import { Metadata, ResolvingMetadata } from "next";
+
+export async function generateMetadata(
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const previousKeywords = (await parent).keywords || [];
+  return {
+    title: `Home | InfoHub`,
+    description: `Explore the intersection of culture, creativity, and technology on InfoHub - the central hub for newsletters, podcasts, and blogs.`,
+    keywords: ["Gen-Z", "Info Hub", ...previousKeywords],
+    metadataBase: new URL("https://trendingstuffs.vercel.app"),
+    twitter: {
+      card: "summary_large_image",
+      title: `Home | InfoHub`,
+      description:
+        "Explore the intersection of culture, creativity, and technology on TrendHub - the central hub for newsletters, podcasts, and blogs.",
+      creator: "@Kinkunmz_",
+      images: [
+        `https://treningstuffs.vercel.app/assets/images/regenerated.jpg`,
+      ],
+    },
+    openGraph: {
+      title: `Home | InfoHub`,
+      description: `Explore the intersection of culture, creativity, and technology on TrendHub - the central hub for newsletters, podcasts, and blogs.`,
+      url: "https://trendingstuffs.vercel.app",
+      siteName: "InfoHub",
+      type: "article",
+      images: [
+        {
+          url: `https://treningstuffs.vercel.app/assets/images/regenerated.jpg`,
+          width: 800,
+          height: 800,
+        },
+        {
+          url: `https://treningstuffs.vercel.app/assets/images/regenerated.jpg`,
+          width: 600,
+          height: 600,
+          alt: "My custom alt",
+        },
+      ],
+    },
+  };
+}
 
 export default function Home({}: {}) {
-  const { blogs, reviews } = pageData;
   return (
     <>
-      <PageLayout page="Home">
-        <div className=" md:container mx-auto px-8 xs:px-0 font-raleway">
-          <Banner
-            heading={{
-              line1: "Articles, Blogs,",
-              line2: "Newsletters & Podcasts.",
-            }}
-            text="Explore our vibrant mix of articles, blogs, newsletters, and podcasts tailored for your curious mind. Stay in the loop with our trendy, informative content, just for you."
-            data={blogs}
-          />
-
-          <Generations />
-        </div>
-
-        <Genres />
-
-        <Collaborative />
-
-        <div className="md:container mx-auto px-8 xs:px-0 font-raleway ">
-          <Review reviews={reviews} />
-
-          <Subscribe />
-        </div>
-      </PageLayout>
+      <HomePageWrapper />
     </>
   );
 }
