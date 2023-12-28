4a76451116b977b994824d1d538bd73f51c90f7d
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-12-28 10:40:54 +0100

    Remove unused code and update BrandLogo component

--- src/components/shared/BrandLogo/BrandLogo.tsx
+++ src/components/shared/BrandLogo/BrandLogo.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import Link from "next/link";
-import { useAppDispatch } from "@/hooks/useAppDispatch";
-import { hideMobileSidebar } from "@/store/slices/mobileSidebar/mobileSidebarSlice";
 
 type Props = {};
 
 const BrandLogo = (props: Props) => {
-  const dispatch = useAppDispatch();
   return (
     <>
-      <Link href="/" onClick={() => dispatch(hideMobileSidebar())}>
+      <Link href="/">
         <button className="font-oleo font-normal text-3xl md:text-4xl text-black dark:text-neutral-200 ">
           Gen-Z
         </button>
